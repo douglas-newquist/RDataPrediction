b16a4539d3b972e7cc1a7fce3899c4ab30155c4c
Douglas Newquist <deathsnake@protonmail.com>
2021-11-27 00:23:56 -0700

    Fxed nan

--- combine.py
+++ combine.py
@@ -6,21 +6,20 @@ import sys
 
 def analyze(row, column, words, columns):
     text = row[column]
-    words = [word in text for word in words]
+    words = [1 if word in text else 0 for word in words]
     return words + [row[c] for c in columns]
 
 
 def main():
-
-    reviews = pd.read_csv(sys.argv[1]).astype(str)
+    reviews = pd.read_csv(sys.argv[1]).fillna('').astype(str)
     frequency = pd.read_csv(sys.argv[2])
 
     column = sys.argv[3]
-    columns = ["country", "points", "price", "province", "region_1",
-               "region_2", "taster_name", "variety", "winery"]
+    columns = ["points", "price"]
+    # columns = ["country", "points", "price", "province", "region_1", "region_2", "taster_name", "variety", "winery"]
     sample_size = int(sys.argv[5]) if len(sys.argv) > 4 else 25
 
-    words = list(frequency["Word"].values[:sample_size])
+    words = list(frequency["Word"].values[: sample_size])
     a = reviews.apply(lambda row: analyze(row, column, words, columns), axis=1)
 
     with open(sys.argv[4], 'w') as result:
